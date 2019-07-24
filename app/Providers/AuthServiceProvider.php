4935dd1994587fab93f75f54d543f2c2a7d5d18d
JasonVV <jasonwangiii@outlook.com>
2019-07-24 23:51:33 +0800

    修改和删除收货地址

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // 使用 Gate::guessPolicyNamesUsing 方法来自定义策略文件的寻找逻辑
+        Gate::guessPolicyNamesUsing(function ($class) {
+            return '\\App\\Policies\\'.class_basename($class).'Policy';
+        });
     }
 }
