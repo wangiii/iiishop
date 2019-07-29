b37af993a9d0ccfcb94314fb539029ad7636d47b
JasonVV <jasonwangiii@outlook.com>
2019-07-29 21:31:56 +0800

    后台商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale', 'rating',
-        'sold_count', 'review_count', 'price'
+        'title', 'description', 'image', 'on_sale',
+        'rating', 'sold_count', 'review_count', 'price'
     ];
 
     protected $casts = [
