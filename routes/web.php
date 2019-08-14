a92557f559e35e0d82a64e57e8e7adfc14ae3269
JasonVV <jasonwangiii@outlook.com>
2019-08-14 22:31:49 +0800

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('products/{product}', 'ProductsController@show')->name('products.show')->where(['product' => '[0-9]+']);
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::post('cart', 'CartController@add')->name('cart.add');
+
 });
