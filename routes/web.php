73e01273aa84c1cab53da67a5ef178aab998745f
JasonVV <jasonwangiii@outlook.com>
2019-08-16 22:41:59 +0800

    Feat: 购物车页面

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,6 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::post('cart', 'CartController@add')->name('cart.add');
-
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
